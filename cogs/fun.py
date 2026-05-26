0d135387b88af4a65480257135931c40d3baaad3
Carlos <ccarrion@its.jnj.com>
2026-05-25 20:54:49 -0500

    Add estupido GIF trigger

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,8 +10,11 @@ class Fun(commands.Cog):
     async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
-        if "rivals" in message.content.lower():
+        content = message.content.lower()
+        if "rivals" in content:
             await message.channel.send("https://tenor.com/view/hop-on-marvel-rivals-marvel-rivals-hop-on-gif-10073567528215660129")
+        if "estupido" in content:
+            await message.channel.send("https://tenor.com/view/howard-hamlin-jimmy-mcgill-saul-goodman-better-call-saul-breaking-bad-gif-22509687")
 
 
 async def setup(bot: commands.Bot):
